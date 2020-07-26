e1ea7b1862dd50b3b25fb3bfee0461b09d3d331f
Rexben <benjamin@eulercoder.com>
2020-07-26 15:45:46 +0100

    calculate route test

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -2,9 +2,49 @@ const request = require('supertest');
 const server = require('../server');
 
 describe('index route', () => {
-  it('API is live!!!', async done => {
+  it('Api is live!!!', async done => {
     const res = await request(server).get('/');
-    expect(res.text).toBe('API is live!!!');
+    expect(res.text).toBe('Api is live!!!');
+    expect(res.status).toBe(200);
+    done();
+  });
+});
+
+describe('calculate route', () => {
+  it('returns 400', async done => {
+    const res = await request(server).post('/api/v1/calculate').send();
+    expect(res.body.message).toBe('Provide the right parameters');
+    expect(res.status).toBe(400);
+    done();
+  });
+  it('returns results of 2', async done => {
+    const res = await request(server)
+      .post('/api/v1/calculate')
+      .send({
+        server: {
+          CPU: 2,
+          RAM: 32,
+          HDD: 100,
+        },
+        VMs: [
+          {
+            CPU: 1,
+            RAM: 16,
+            HDD: 10,
+          },
+          {
+            CPU: 1,
+            RAM: 16,
+            HDD: 10,
+          },
+          {
+            CPU: 2,
+            RAM: 32,
+            HDD: 100,
+          },
+        ],
+      });
+    expect(res.body.result).toBe(2);
     expect(res.status).toBe(200);
     done();
   });
